2d1fdaacd63f302b007100b02c94e5751bd3eedc
Dmitry Medyannik <mail@artdima.ru>
2020-08-08 22:26:40 +0300

    Add multiplatform project

---
+++ MultiplatformAppSwiftUI/Shared/MultiplatformAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformAppSwiftUIApp.swift
+//  Shared
+//
+//  Created by Medyannik Dima on 08.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
